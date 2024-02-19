29f01c7389cb2b9cea0ed5d1b5a6027e1cdf0c77
josh dejeu <dejeujosh@gmail.com>
2024-02-19 00:05:42 +0000

    init

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,35 @@
 import numpy as np
 
+
+# mean, variance, standard deviation, max, min, and sum of the rows, columns, and elements
 def calculate(list):
+  if (len(list) < 9):
+    raise ValueError("List must contain nine numbers.")
 
+  dt = np.array(list).reshape(3, 3)
 
+  means = [
+      dt.mean(axis=0).tolist(),
+      dt.mean(axis=1).tolist(),
+      dt.mean().tolist()
+  ]
+  variances = [
+      dt.var(axis=0).tolist(),
+      dt.var(axis=1).tolist(),
+      dt.var().tolist()
+  ]
+  stds = [dt.std(axis=0).tolist(), dt.std(axis=1).tolist(), dt.std().tolist()]
+  maxs = [dt.max(axis=0).tolist(), dt.max(axis=1).tolist(), dt.max().tolist()]
+  mins = [dt.min(axis=0).tolist(), dt.min(axis=1).tolist(), dt.min().tolist()]
+  sums = [dt.sum(axis=0).tolist(), dt.sum(axis=1).tolist(), dt.sum().tolist()]
 
+  calculations = {
+      'mean': means,
+      'variance': variances,
+      'standard deviation': stds,
+      'max': maxs,
+      'min': mins,
+      'sum': sums
+  }
 
-    return calculations
+  return calculations
